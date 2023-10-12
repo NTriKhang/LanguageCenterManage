94299dac3a43c775f8337de7b04f58bed35a0f8a
NguyenTriKhang <rokkhangnvd@outlook.com.vn>
2023-10-12 21:46:15 +0700

    add state to course

---
+++ LanguageCenterManage/utility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageCenterManage
+{
+    public class utility
+    {
+        public static string CourseOpen { get; } = "CourseOpen";
+        public static string CourseProcessing { get; } = "CourseProcessing";
+        public static string CourseClose { get; } = "CourseClose";
+    }
+}
